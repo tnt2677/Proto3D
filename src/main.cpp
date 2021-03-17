6ba7fb26c636cfcb9c129dafef4a08527a610349
Tanishk Tiwari <tatiwari@microsoft.com>
2021-03-17 21:48:02 +0530

    blend a color and a texture

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
+#include "stb_image.h"
 #include "util.h"
 
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -102,8 +104,7 @@ void process_input(GLFWwindow *window) {
     glfwSetWindowShouldClose(window, true);
 }
 
-void setup_debug(bool enable)
-{
+void setup_debug(bool enable) {
   glDebugMessageCallbackKHR(enable ? gl_debug_logger : nullptr, stderr);
   glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS_KHR);
   if (GL_NO_ERROR != glGetError())
@@ -126,10 +127,9 @@ int main() {
 
   constexpr auto INIT_WIDTH = 800u;
   constexpr auto INIT_HEIGHT = 600u;
-  GLFWwindow* window = glfwCreateWindow(INIT_WIDTH, INIT_HEIGHT,
-                                        "Learn OpenGL", nullptr, nullptr);
-  if (!window)
-  {
+  GLFWwindow *window = glfwCreateWindow(INIT_WIDTH, INIT_HEIGHT, "Learn OpenGL",
+                                        nullptr, nullptr);
+  if (!window) {
     std::cout << "Failed to create GLFW window\n";
     glfwTerminate();
     return -1;
@@ -137,69 +137,125 @@ int main() {
   glfwMakeContextCurrent(window);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-  {
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD\n";
     return -1;
   }
 #ifndef NDEBUG
-  if (GLAD_GL_KHR_debug)
-  {
+  if (GLAD_GL_KHR_debug) {
     setup_debug(true);
   }
 #endif
   glViewport(0, 0, INIT_WIDTH, INIT_HEIGHT);
   glClearColor(0.188f, 0.349f, 0.506f, 1.0f);
 
-  float positions[18] = {// positions         // colors
-                         0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-                         -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-                         0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f};
-
-  // unsigned int indices[6] = {
-  //     // note that we start from 0!
-  //     0, 1, 3, // first triangle
-  //     1, 2, 3  // second triangle
-  // };
-
-  unsigned int vertex_array;
-  glGenVertexArrays(1, &vertex_array);
-  glBindVertexArray(vertex_array);
-
-  unsigned int buffer;
-  glGenBuffers(1, &buffer);
-  glBindBuffer(GL_ARRAY_BUFFER, buffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-
-  // unsigned int EBO;
-  // glGenBuffers(1, &EBO);
-  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-  //              GL_STATIC_DRAW);
-
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0 /*index*/, 3 /*size*/, GL_FLOAT,
-                        GL_FALSE /*normalised*/, sizeof(float) * 6 /*stride*/,
-                        (void *)0 /*attribute pointer*/);
-
-  glEnableVertexAttribArray(1);
-  glVertexAttribPointer(1 /*index*/, 3 /*size*/, GL_FLOAT,
-                        GL_FALSE /*normalised*/, sizeof(float) * 6 /*stride*/,
-                        (void *)(3 * sizeof(float)) /*attribute pointer*/);
-
+  // Build and compile our shader program :
   ShaderProgramSource source = parse_shader("src/res/shaders/basic.shader");
   unsigned int shader_program =
       create_shaders(source.vertex_source, source.fragment_source);
 
   glUseProgram(shader_program);
 
+  // set up vertex data (and buffer(s)) and configure vertex attributes
+  // ------------------------------------------------------------------
+  float vertices[] = {
+      // positions          // colors           // texture coords
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+  };
+  unsigned int indices[] = {
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
+  };
+
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  // texture coord attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  // load and create a texture
+  // -------------------------
+  unsigned int texture1;
+  // texture 1
+  // ---------
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+
+  // set the texture wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // load image, create texture and generate mipmaps
+  int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(
+      true); // tell stb_image.h to flip loaded texture's on the y-axis.
+
+  unsigned char *data = stbi_load("src/res/textures/wooden_texture.jpeg",
+                                  &width, &height, &nrChannels, 0);
+
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  // tell opengl for each sampler to which texture unit it belongs to (only has
+  // to be done once)
+  // don't forget to activate/use the shader before setting uniforms!
+
+  glUniform1i(glGetUniformLocation(shader_program, "texture1"), 0);
+
   while (!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
+    // input
+    // -----
     process_input(window);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    // render
+    // ------
+    glClear(GL_COLOR_BUFFER_BIT);
 
+    // bind Texture
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    // render container
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+    // etc.)
+    // -------------------------------------------------------------------------------
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
--- src/res/shaders/basic.shader
+++ src/res/shaders/basic.shader
@@ -1,22 +1,30 @@
 #shader vertex
 #version 330 core
-layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
-layout (location = 1) in vec3 aColor; // the color variable has attribute position 1
-  
-out vec3 ourColor; // output a color to the fragment shader
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
+
+out vec3 ourColor;
+out vec2 TexCoord;
 
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
-    ourColor = aColor; // set ourColor to the input color we got from the vertex data
-}       
+	gl_Position = vec4(aPos, 1.0);
+	ourColor = aColor;
+	TexCoord = vec2(aTexCoord.x, aTexCoord.y);
+}
 
 #shader fragment
 #version 330 core
-out vec4 FragColor;  
+out vec4 FragColor;
+
 in vec3 ourColor;
-  
+in vec2 TexCoord;
+
+// texture samplers
+uniform sampler2D texture1;
+
 void main()
 {
-    FragColor = vec4(ourColor, 1.0);
+	FragColor = texture(texture1, TexCoord)*vec4(ourColor,1.0);
 }
